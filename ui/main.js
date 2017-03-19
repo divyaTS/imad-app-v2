b52efdc10208647dfda47b046e57c59000b344eb
divyaTS <dhivyanivetha1@gmail.com>
2017-03-19 02:27:13 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,17 +2,25 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
-    //make a request to the counter endpoint
+    //create a request object
+    var request=new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //take some action
+            if(request.status==200){
+                var counter=request.resonseText();
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //not done yet
+    };
     
-    //render the variable in the correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
-    
-    
-    
+    //Make a request
+    request.open('GET','http://divyats.imad.hasura-app.io/counter',true);
+    request.send(null);
     
 };
